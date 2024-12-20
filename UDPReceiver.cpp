b144db6ba6fc27e9ee1cea423125bfeb7a0b62b0
BiZaRrE96 <91012945+BiZaRrE96@users.noreply.github.com>
2024-12-20 11:36:49 +0700

    New version

--- UDPReceiver.cpp
+++ UDPReceiver.cpp
@@ -3,13 +3,14 @@
 #include <arpa/inet.h>
 #include <unistd.h>
 
-#define PORT 8080
+#define RECV_PORT 8080
+#define SEND_PORT 6969
 #define BUF_SIZE 1024
 
 int main() {
     int sockfd;
     char buffer[BUF_SIZE];
-    struct sockaddr_in server_addr, client_addr;
+    struct sockaddr_in recv_addr, send_addr, client_addr;
 
     // Create socket
     if ((sockfd = socket(AF_INET, SOCK_DGRAM, 0)) < 0) {
@@ -17,20 +18,26 @@ int main() {
         return 1;
     }
 
-    // Fill server information
-    memset(&server_addr, 0, sizeof(server_addr));
-    server_addr.sin_family = AF_INET;
-    server_addr.sin_addr.s_addr = INADDR_ANY;
-    server_addr.sin_port = htons(PORT);
+    // Fill receiver information
+    memset(&recv_addr, 0, sizeof(recv_addr));
+    recv_addr.sin_family = AF_INET;
+    recv_addr.sin_addr.s_addr = INADDR_ANY;
+    recv_addr.sin_port = htons(RECV_PORT);
 
     // Bind the socket
-    if (bind(sockfd, (const struct sockaddr *)&server_addr, sizeof(server_addr)) < 0) {
+    if (bind(sockfd, (const struct sockaddr *)&recv_addr, sizeof(recv_addr)) < 0) {
         perror("Bind failed");
         close(sockfd);
         return 1;
     }
 
-    std::cout << "Receiver ready. Listening on port " << PORT << "...\n";
+    std::cout << "Receiver ready. Listening on port " << RECV_PORT << "...\n";
+
+    // Fill sender information
+    memset(&send_addr, 0, sizeof(send_addr));
+    send_addr.sin_family = AF_INET;
+    send_addr.sin_port = htons(SEND_PORT);
+    send_addr.sin_addr.s_addr = inet_addr("127.0.0.1");
 
     socklen_t len = sizeof(client_addr);
     while (true) {
@@ -43,6 +50,14 @@ int main() {
         }
 
         std::cout << "Received message: " << buffer << "\n";
+
+        // Respond back
+        std::string reply = "Acknowledged: " + std::string(buffer);
+        if (sendto(sockfd, reply.c_str(), reply.length(), MSG_CONFIRM,
+                   (const struct sockaddr *)&send_addr, sizeof(send_addr)) < 0) {
+            perror("Send failed");
+            break;
+        }
     }
 
     close(sockfd);
--- UDPSender.cpp
+++ UDPSender.cpp
@@ -3,13 +3,14 @@
 #include <arpa/inet.h>
 #include <unistd.h>
 
-#define PORT 8080
+#define SEND_PORT 6969
+#define RECV_PORT 8080
 #define BUF_SIZE 1024
 
 int main() {
     int sockfd;
     char buffer[BUF_SIZE];
-    struct sockaddr_in server_addr;
+    struct sockaddr_in recv_addr, send_addr;
 
     // Create socket
     if ((sockfd = socket(AF_INET, SOCK_DGRAM, 0)) < 0) {
@@ -17,25 +18,52 @@ int main() {
         return 1;
     }
 
-    memset(&server_addr, 0, sizeof(server_addr));
+    // Fill sender information
+    memset(&send_addr, 0, sizeof(send_addr));
+    send_addr.sin_family = AF_INET;
+    send_addr.sin_addr.s_addr = INADDR_ANY;
+    send_addr.sin_port = htons(SEND_PORT);
 
-    // Fill server information
-    server_addr.sin_family = AF_INET;
-    server_addr.sin_port = htons(PORT);
-    server_addr.sin_addr.s_addr = inet_addr("127.0.0.1");
-
-    std::cout << "Enter message to send: ";
-    std::cin.getline(buffer, BUF_SIZE);
-
-    // Send message
-    if (sendto(sockfd, buffer, strlen(buffer), MSG_CONFIRM,
-               (const struct sockaddr *)&server_addr, sizeof(server_addr)) < 0) {
-        perror("Send failed");
+    // Bind the socket
+    if (bind(sockfd, (const struct sockaddr *)&send_addr, sizeof(send_addr)) < 0) {
+        perror("Bind failed");
         close(sockfd);
         return 1;
     }
 
-    std::cout << "Message sent: " << buffer << "\n";
+    std::cout << "Sender ready. Bound to port " << SEND_PORT << ".\n";
+
+    // Fill receiver information
+    memset(&recv_addr, 0, sizeof(recv_addr));
+    recv_addr.sin_family = AF_INET;
+    recv_addr.sin_port = htons(RECV_PORT);
+    recv_addr.sin_addr.s_addr = inet_addr("127.0.0.1");
+
+    while (true) {
+        // Send message
+        std::cout << "Enter message to send: ";
+        std::cin.getline(buffer, BUF_SIZE);
+
+        if (sendto(sockfd, buffer, strlen(buffer), MSG_CONFIRM,
+                   (const struct sockaddr *)&recv_addr, sizeof(recv_addr)) < 0) {
+            perror("Send failed");
+            break;
+        }
+
+        std::cout << "Message sent: " << buffer << "\n";
+
+        // Receive response
+        memset(buffer, 0, BUF_SIZE);
+        socklen_t len = sizeof(recv_addr);
+        int n = recvfrom(sockfd, buffer, BUF_SIZE, MSG_WAITALL,
+                         (struct sockaddr *)&recv_addr, &len);
+        if (n < 0) {
+            perror("Receive failed");
+            break;
+        }
+
+        std::cout << "Received response: " << buffer << "\n";
+    }
 
     close(sockfd);
     return 0;
